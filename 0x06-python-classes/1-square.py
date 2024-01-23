54f02bfaf5032d5e9dfd5a90645bfdb6c53e1deb
JalalErr <jalal.errajy@gmail.com>
2024-01-23 21:42:31 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Declaration of class Square"""
+
+
+class Square:
+    """Inside a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square,
+        Args:
+            size (int): The size of the new square"""
+        self._size = size
