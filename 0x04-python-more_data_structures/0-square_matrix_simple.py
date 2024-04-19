c45ffa3fdd55cf44e516add0f7b3edfcd4c8c0d6
JalalErr <jalal.errajy@gmail.com>
2024-04-19 16:53:45 +0100

    fixing code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,16 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """_summary_
+
+    Args:
+        matrix (list).
+
+    Returns:
+        _type_: new_matrix : the results of  multiplying each element in matrix.
+    """
     new_matrix = [0] * len(matrix)
+    if len(matrix) == 0:
+        return new_matrix
     for i in range(0, len(matrix)):
         for j in range(0, i):
             new_matrix[i][j] = matrix[i][j] * matrix[i][j]
